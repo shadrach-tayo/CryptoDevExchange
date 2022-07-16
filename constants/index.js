697d76e2622f51e18da0aa274ef45fff0021d75e
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-07-16 05:14:31 +0100

    test crypto exchange ui

---
+++ constants/index.js
@@ -0,0 +1,7 @@
+import ExchangeContract from "../abis/Exchange.json";
+import CryptoDevsToken from "../abis/CryptoDevToken.json";
+
+export const TOKEN_CONTRACT_ABI = CryptoDevsToken.abi;
+export const TOKEN_CONTRACT_ADDRESS = "0x96d650DDe057BA10A35C04D50bBe123FF4543D88";
+export const EXCHANGE_CONTRACT_ABI = ExchangeContract.abi;
+export const EXCHANGE_CONTRACT_ADDRESS = "0x962f69610932D221ae80c102FA87b9dDfA5ec94c";
